3c86f37db12cde39c455d263aec7254858c19c57
Vasiliev Sergei <sergei.binp@gmail.com>
2023-09-03 15:12:13 +0500

    Initial commit

---
+++ src/main/java/SeminarOne.java
@@ -0,0 +1,20 @@
+public class SeminarOne {
+    public static void main(String[] args) {
+        System.out.println("Yo!");
+        // Строгая типизация
+            // Примитивы
+        int i = 'e';
+        long l;
+        float f;
+        double d;
+        byte bt = 'a';
+        short s;
+        boolean b;
+        char ch = 'F';
+            // Ссылочные
+        int[] ints;
+        String str;
+
+
+    }
+}
